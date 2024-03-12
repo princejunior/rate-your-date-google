b2b45103c44d643bf24b087e4180049f3779724d
Elijah Elliott <elliott.t.elijah@gmail.com>
2024-03-12 09:04:53 -0600

    first commit

---
+++ rmd_web/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.core.exceptions import MultipleObjectsReturned
+from django.http import HttpResponseServerError
+
+# def my_view(request):
+#     try:
+#         # Code that may raise MultipleObjectsReturned exception
+#         # ...
+#     except MultipleObjectsReturned as e:
+#         # Log the exception or handle it gracefully
+#         # For example, you can return an error response to the user
+#         return HttpResponseServerError("Multiple objects returned. Please contact the administrator.")
+
+# Create your views here.
+def profile(request):
+    return render(request, 'profile/profile.html')
