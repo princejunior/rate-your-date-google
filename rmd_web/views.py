2158b7dfd36f19505987425da2521462e92c2877
Elijah Elliott <elliott.t.elijah@gmail.com>
2024-03-15 00:21:56 -0600

    added more pages and url path

--- rmd_web/views.py
+++ rmd_web/views.py
@@ -10,7 +10,7 @@ from .static.functions.fire import create_user_profile, edit_user_profile
 from .static.functions.friend_request import send_friend_request
 from .static.functions.search import search_users
 from .static.functions.friend_request import send_friend_request
-
+from .static.functions.search import search_users
 # from .static.functions.ads import
 # from .static.functions.friend_request import
 # from .static.functions.group_events import
@@ -37,10 +37,16 @@ def profile(request):
     return render(request, 'profile/profile.html')
 
 def get_individuals_profile(request):
+def get_individuals_profile(request):
     print(request.user)
     return render(request, 'profile/profile.html')
     
 def create_user(request):
+    email = request.user.email
+    social_media = [
+       request.POST.get('instagram'),
+       request.POST.get('facebook'),
+    ]
     email = request.user.email
     social_media = [
        request.POST.get('instagram'),
@@ -54,13 +60,27 @@ def create_user(request):
             'profile_picture': request.POST.get('profile_picture'),
             'professional_background': request.POST.getlist('professional_background'),
             'social_media': social_media,
+            'email': email,
+            'first_name': request.POST.get('first_name'),
+            'last_name': request.POST.get('last_name'),
+            'profile_picture': request.POST.get('profile_picture'),
+            'professional_background': request.POST.getlist('professional_background'),
+            'social_media': social_media,
             'interests': request.POST.getlist('interests'),
+            'privacy_settings': {'email_visibility': request.POST.get('email_visibility')}
             'privacy_settings': {'email_visibility': request.POST.get('email_visibility')}
         }
         create_user_profile(user_profile_data)
         return redirect('profile_created')  # Redirect to a page indicating profile creation success
     return render(request, 'profile/create_user.html')
 
+def edit_user(request):
+    print(request.user.id)
+    social_media = [
+       request.POST.get('instagram'),
+       request.POST.get('facebook'),
+    ]
+    user_id = request.user.id
 def edit_user(request):
     print(request.user.id)
     social_media = [
@@ -75,9 +95,16 @@ def edit_user(request):
             'profile_picture': request.POST.get('profile_picture'),
             'professional_background': request.POST.getlist('professional_background'),
             'social_media': social_media,
+            'first_name': request.POST.get('first_name'),
+            'last_name': request.POST.get('last_name'),
+            'profile_picture': request.POST.get('profile_picture'),
+            'professional_background': request.POST.getlist('professional_background'),
+            'social_media': social_media,
             'interests': request.POST.getlist('interests'),
             'privacy_settings': {'email_visibility': request.POST.get('email_visibility')}
+            'privacy_settings': {'email_visibility': request.POST.get('email_visibility')}
         }
+        edit_user_profile(user_id, updated_data)
         edit_user_profile(user_id, updated_data)
         return redirect('profile_edited')  # Redirect to a page indicating profile edit success
     return render(request, 'profile/edit_user.html')
@@ -102,6 +129,7 @@ def search_results(request):
         results = []
     return render(request, 'pages/search.html', {'results': results, 'query': query})
 # # views.py
+# FUNCTIONALITY PAGES
 
 def send_friend_request_view(request):
     if request.method == 'POST':
@@ -110,7 +138,13 @@ def send_friend_request_view(request):
         send_friend_request(sender_id, receiver_id)
         # Add appropriate response or redirect here
 
-
+def search_results(request):
+    query = request.GET.get('query')
+    if query:
+        results = search_users(query)
+    else:
+        results = []
+    return render(request, 'pages/search.html', {'results': results, 'query': query})
 ######################################################################################################################
 # 
 
